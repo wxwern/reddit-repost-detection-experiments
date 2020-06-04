03530487ffa7a1bb16d56469a7600494cf764ca2
Aria <65660580+ariasymphony@users.noreply.github.com>
2020-06-04 13:15:05 +0800

    very basic ocr
    it also edits text for hopefully easier comparison jic ppl's formatting is different
    
    No editing images rn because so far all edits have made ocr worse

--- ocr/ocr.py
+++ ocr/ocr.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
 
+from PIL import Image
+import pytesseract
+
 class OCR:
+
+    def __init__(self):
+        pass
+
+    def read(meme):
+        rawtext = pytesseract.image_to_string(meme)
+        nlsplit = rawtext.split("\n")
+        final = ""
+        for i in nlsplit:
+            edit = i.split(" ")
+            newstring = ""
+            for g in edit:
+                if g != "":
+                    newstring = newstring + g + " "
+            final = final + newstring
+            
+        
+        return final
+
+    
     pass
