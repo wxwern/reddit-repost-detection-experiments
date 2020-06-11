e28a28ebec5598218a0670f16530446aeffcd2d2
Aria <65660580+ariasymphony@users.noreply.github.com>
2020-06-11 12:56:54 +0800

    testing file prints original text now
    formatted to look nicer
    prints original then edited of both original image and grayscale
    
    "what is a meme?" text disappears using this for some reason

--- ocr/Testing/testocr.py
+++ ocr/Testing/testocr.py
@@ -7,8 +7,12 @@ class TestOCR:
     def __init__(self):
         pass
 
-    def read(meme):
+    def read(meme, original: bool):
+        '''with added original text feature! :D'''
         rawtext = pytesseract.image_to_string(meme)
+        if original:
+            return rawtext
+        rawtext = rawtext.replace("-\n", "")
         nlsplit = rawtext.split("\n")
         final = ""
         for i in nlsplit:
@@ -30,9 +34,16 @@ class TestOCR:
 def testImage(name):
     ocr = TestOCR
     image = Image.open(name)
-    image.show()
-    print(ocr.read(image))
     grayscale = ImageOps.grayscale(image)
-    grayscale.show()
-    print(ocr.read(grayscale))
+    print("Original:")
+    print(ocr.read(image, True))
+    print("-----------------------------------")
+    print(ocr.read(image, False))
+    print("=======================================================")
+    print("Grayscale original:")
+    print(ocr.read(grayscale, True))
+    print("-----------------------------------")
+    print(ocr.read(grayscale, False))
+    
+    
     
