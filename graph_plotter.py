eb50ef9116add06612e72efc3533215d617c1230
Wern Jie Lim <lwjdev@gmail.com>
2020-10-10 21:01:16 +0800

    updated graph plotter for normalized val

--- graph_plotter.py
+++ graph_plotter.py
@@ -21,8 +21,8 @@ while True:
             print('error: sample count mismatch')
         print('input another computed graph json file name (type nothing to graph):')
 
-def compute_color(i_diff,t_sim):
-    return np.array([max(min(1.0, 1-i_diff/20),0.0)*0.75, 0, max(min(1.0,t_sim),0.0)*0.75, 1.0])
+def compute_color(i_sim,t_sim):
+    return np.array([max(min(1.0,i_sim),0.0)*0.75, 0, max(min(1.0,t_sim),0.0)*0.75, 1.0])
 
 plt.xlabel('precision')
 plt.ylabel('recall')
@@ -33,13 +33,14 @@ axes.set_ylim([0.0,1.0])
 
 full_list = []
 
-img_range = range(0, 20+1, 2)
+img_range = list(set(map(lambda x: x['img_sim_min'], d['data'])))
+img_range.sort()
 for m in img_range:
     points = list(map(lambda x: \
-                      (x['results']['precision'], x['results']['recall'], x['img_diff_min'], x['text_sim_min'], x['results']['f1_score']), \
+                      (x['results']['precision'], x['results']['recall'], x['img_sim_min'], x['text_sim_min'], x['results']['f1_score']), \
                       sorted(
                           list( \
-                              filter(lambda x: x['img_diff_min'] == m, d['data']) \
+                              filter(lambda x: x['img_sim_min'] == m, d['data']) \
                           ), \
                           key=lambda x: x['text_sim_min']
                       )
@@ -54,7 +55,7 @@ full_list.sort(key=lambda x: (x[-1], x[0]), reverse=True)
 print()
 print("Most feasible points")
 for x in full_list[0:5]:
-    print("img_diff_min: %d, text_sim_min: %.2f" % (x[2], x[3]))
+    print("img_sim_min: %.3f, text_sim_min: %.3f" % (x[2], x[3]))
     print("precision: %.4f, recall: %.4f" % (x[0], x[1]))
     print("f1_score: %.4f" % x[4])
     print("---")
